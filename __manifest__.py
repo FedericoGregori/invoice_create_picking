b2228b143ef5bd274d3f17a638578f5c1a8beaff
morrillo <morrillo@umich.edu>
2020-06-22 11:22:12 +0000

    First commit

---
+++ __manifest__.py
@@ -0,0 +1,24 @@
+{
+    "name": "Crea remito desde factura",
+    'version': '13.0.1.3.0',
+    'category': 'Accounting',
+    'sequence': 14,
+    'license': 'AGPL-3',
+    'summary': '',
+    'depends': [
+        'account',
+        'stock',
+    ],
+    'external_dependencies': {
+    },
+    'data': [
+        'views/account_view.xml',
+    ],
+    'demo': [
+    ],
+    'images': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
